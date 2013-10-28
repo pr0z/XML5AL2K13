9416d219b68dc4891a16a1e8f806a7d57eb5bb67
quentin-libert <libert.quentin@gmail.com>
2013-10-28 14:26:12 +0100

    Update finalstep.php
    first test write xml file

--- finalstep.php
+++ finalstep.php
@@ -3,6 +3,18 @@ session_start();
 $nbtables = $_GET['nbtables'];
 $tables = $_SESSION['tables'];
 
+$user_name = "user-name";
+$db_name = $_SESSION['dbname'];
+
+// variable contenant le xml
+$xml_script = "<?xml version=\"1.0\" encoding=\"ISO-8859-1\" ?>\r\n";
+$xml_script .= "\r<database>\r\n";
+$xml_script .= "\t<metaInformations>\r\n";
+$xml_script .= "\t\t<dbName>$db_name</dbName>\r\n";
+$xml_script .= "\t\t<creatorName>$user_name</creatorName>\r\n";
+$xml_script .= "\t\t<creationDate>$_SESSION['dbname']</creationDate>\r\n";
+$xml_script .= "\t</metaInformations>\r\n";
+
 $colsByTable = array();
 for ($i = 0; $i < $nbtables; $i++ ){
     $id = $id+1;
@@ -25,7 +37,15 @@ for ($i = 0; $i < $nbtables; $i++ ){
 }
 
 echo "<pre>";
-print_r($colsByTable);     
+print_r($colsByTable);
+
+$xml_script .= "\r</database>\r\n";
+
+// Ecriture du fichier XML
+$file = fopen("xml/$user_name/$db_name.xml", "w+");
+fwrite($file, $_xml);
+fclose($file);
+
 echo "</pre>";
 session_destroy();
 ?>
