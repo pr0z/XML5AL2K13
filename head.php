3c3429a161933111b9c71a6484ca88d53ddce4d2
quentin-libert <libert.quentin@gmail.com>
2013-11-12 14:13:38 +0100

    Update head.php

--- head.php
+++ head.php
@@ -5,6 +5,7 @@
 		<META HTTP-EQUIV="Content-Type" CONTENT="text/html;charset=UTF-8">
 		<link rel="stylesheet" href="CSS/style.css" type="text/css"/>
 		<link rel="icon" type="image/x-icon" href="images/icon.jpg" />
+		<script type="text/javascript" src="js/jquery-1.10.2.min.js"></script>
 	<head>
 <body>
 	<div id="head">
