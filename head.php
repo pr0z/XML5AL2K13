9bf76861f4a928873831be658ec233db8652b985
quentin-libert <libert.quentin@gmail.com>
2013-11-15 11:03:20 +0100

    Update head.php

--- head.php
+++ head.php
@@ -4,9 +4,11 @@
 		<title>Projet XML</title>	
 		<META HTTP-EQUIV="Content-Type" CONTENT="text/html;charset=UTF-8">
 		<link rel="stylesheet" href="CSS/style.css" type="text/css"/>
+		<link rel="stylesheet" href="CSS/jquery-ui.css.css" type="text/css"/>
 		<link rel="icon" type="image/x-icon" href="images/xmlIcon.png" />
 		<script type="text/javascript" src="js/jquery-1.10.2.min.js"></script>
                 <script type="text/javascript" src="js/verifChamps.js"></script>
+                <script type="text/javascript" src="js/jquery-ui.js"></script>
 	<head>
 <body>
 	<div id="head">
